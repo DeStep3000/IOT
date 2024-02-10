e776245be81ee3e83bc071455711105952e8ad41
limoncatGod <dabholak@gmail.com>
2024-02-10 20:09:32 +0300

    added method is_point_in

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ struct Point {
 
 class Polygon{
 private:
-    std::vector<Point> vertices;
+    std::vector<Point> vertices; // array of vertex
+    int num_vertices = 0; // lengt of array
 public:
     Polygon(){} //using standart
     std::vector<Point> get_vertices(){
@@ -16,21 +17,40 @@ public:
     void print_vertices(){
         for(Point p: this->vertices){
             std::cout << p.x << " " << p.y << "\t";
-            std::cout << "\n";
         }
+        std::cout << "\n";
     }
     void add_vertex(Point point){
         this->vertices.push_back(point);
+        this->num_vertices += 1;
+    }
+    bool is_point_in(Point point){ //checking is point in polygon (rewrite because of points on edge
+        bool result = false;
+        int j = this->num_vertices - 1;
+        for(int i = 0; i < this->num_vertices - 1; i++){
+            if ( (this->vertices[i].y < point.y && this->vertices[j].y >= point.y || this->vertices[j].y < point.y && this->vertices[i].y >= point.y) &&
+                 (this->vertices[i].x + (point.y - this->vertices[i].y) / (this->vertices[j].y - this->vertices[i].y) * (this->vertices[j].x - this->vertices[i].x) < point.x) )
+                result = !result;
+            j = i;
+        }
+        return result;
     }
 };
 
 int main() {
     Polygon pn1;
-    Point a{2, 3};
-    Point b{3, 4};
+    Point a{1, 1};
+    Point b{3, 5};
+    Point c{9, 4};
+    Point d{11, 3};
+    Point e{6, 3};
     pn1.add_vertex(a);
     pn1.add_vertex(b);
+    pn1.add_vertex(c);
+    pn1.add_vertex(d);
+    pn1.add_vertex(e);
     pn1.print_vertices();
-    std::cout << "Hello, World!" << std::endl;
+    Point check_point{9, 4};
+    std::cout << pn1.is_point_in(check_point) << std::endl;
     return 0;
 }
