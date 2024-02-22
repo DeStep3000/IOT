324d0d4f04b0764a0299325eea3ebd5e88f87a4f
Андрей <elkabor@mail.ru>
2024-02-22 12:47:18 +0300

    comments+mycode

--- readfile.h
+++ readfile.h
@@ -6,7 +6,7 @@
 #include <vector>
 
 const std::string KEY = "03";//word that used for cutting coords
-const std::string NUMBERS = "0123456789";
+const std::string NUMBERS = "0123456789";//numbers that used to find first number after key
 
 std::string read_file(const std::string& path);
 long int find_key(const std::string& path, long int index=0);// changed to long int
