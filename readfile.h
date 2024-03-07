1d80be0a994edb192cbc94e15b7f1021fb4bedf0
Андрей <elkabor@mail.ru>
2024-03-07 17:25:10 +0300

    small commit(need to test all readfiles)

--- readfile.h
+++ readfile.h
@@ -9,7 +9,9 @@ const std::vector<std::string> KEY = {" 03 "," 04 "," 05 "," 06 "," 07 "," 08 ",
 const std::string NUMBERS = "0123456789";//numbers that used to find first number after key
 
 std::string read_file(const std::string& path);
+std::size_t find_min_key(const std::string input,std::size_t index);
 std::size_t find_key(const std::string& path, std::size_t index=0);
+std::vector<double> fill_coords_from_input(const std::string& input,std::size_t previous_index);
 std::vector<double> get_coords(const std::string& path, std::size_t index=0);
 
 #endif
--- tests.cpp
+++ tests.cpp
@@ -2,9 +2,10 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-
-/*int main(int argc, char **argv)
-{
+/*TEST(find_min_key_test,differant inputs){
+    ASSERT_TRUE(find_min_key)
+}*/
+/*int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::InitGoogleMock(&argc, argv);
 
