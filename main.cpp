efdf55eecf61ec63429d07debde256256664adea
Андрей <elkabor@mail.ru>
2024-03-19 14:26:03 +0300

    edit_file tests+ future tests

--- main.cpp
+++ main.cpp
@@ -383,9 +383,9 @@ public:
     }
 };
 
-
+/*
 int main() {
-    /*const std::string path = "";//Absolute file path
+    const std::string path = "E:\\clion\\IOT4\\test2.txt";//Absolute file path
     std::string input = read_file(path);
     input= edit_file(input);
 
@@ -393,6 +393,6 @@ int main() {
     pn_field.input_polygons(input);
     pn_field.print_field();
     Polygon res = pn_field.intersect_polygon_field_final();
-    res.print_vertices();*/
+    res.print_vertices();
     return 0;
-}
+}*/
