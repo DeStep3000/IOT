656000967389df879164c2d4c951046da5ed155f
DeStep <tankt34.stepanoff@yandex.ru>
2024-03-19 15:43:41 +0300

    it's working

--- main.cpp
+++ main.cpp
@@ -383,9 +383,9 @@ public:
     }
 };
 
-/*
+
 int main() {
-    const std::string path = "E:\\clion\\IOT4\\test2.txt";//Absolute file path
+    const std::string path = "C:\\DeStep\\HSE\\C++\\IOT\\test2.txt";//Absolute file path
     std::string input = read_file(path);
     input= edit_file(input);
 
@@ -395,4 +395,4 @@ int main() {
     Polygon res = pn_field.intersect_polygon_field_final();
     res.print_vertices();
     return 0;
-}*/
+}
