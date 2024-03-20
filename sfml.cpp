b7fa62170760bca11a9e814eb6e59816d1d7fee5
DeStep <tankt34.stepanoff@yandex.ru>
2024-03-20 17:55:15 +0300

    create grid

--- sfml.cpp
+++ sfml.cpp
@@ -1,6 +1,31 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+sf::VertexArray coord_grid(sf::RenderWindow& win, int rows, int cols){
+    // initialize values
+    int numLines = rows+cols-2;
+    sf::VertexArray grid(sf::Lines, 2*(numLines));
+    win.setView(win.getDefaultView());
+    auto size = win.getView().getSize();
+    float rowH = size.y/rows;
+    float colW = size.x/cols;
+    // row separators
+    for(int i=0; i < rows-1; i++){
+        int r = i+1;
+        float rowY = rowH*r;
+        grid[i*2].position = {0, rowY};
+        grid[i*2+1].position = {size.x, rowY};
+    }
+    // column separators
+    for(int i=rows-1; i < numLines; i++){
+        int c = i-rows+2;
+        float colX = colW*c;
+        grid[i*2].position = {colX, 0};
+        grid[i*2+1].position = {colX, size.y};
+    }
+    return grid;
+}
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
     sf::CircleShape shape(100.f);
@@ -13,8 +38,9 @@ int main() {
                 window.close();
         }
 
-        window.clear();
+        window.clear(sf::Color::Blue);
         window.draw(shape);
+        window.draw(coord_grid(window, 10, 10));
         window.display();
     }
 
