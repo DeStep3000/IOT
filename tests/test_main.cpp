f32a34ee33ccf4c6c6e3d7a74e4713bd712da512
Андрей <elkabor@mail.ru>
2024-03-20 15:20:04 +0300

    tests+++

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
+//#include <gmock/gmock.h>
 #include "../main.h"
 
+// test: mult_vector,intersection, is_intersected,all class polygon functions
+// test: operator overloading, length, sign, abs
+//main test #14 intersect_polygon_field_final
 TEST(input_polygons,test1){
     std::string input=" 03 9 56 75259 259 25 27 03 10 567 4 23 75259 259 25 27 03 11 567 4 23 75259 259 67 47 47 57 05 12 567 4 23 75259 259 67 68 39 988 90 67 03 03 13 567 4 23 5 03 14 03 34 34 ";
     PolygonField pn_field;
     pn_field.input_polygons(input);
     EXPECT_EQ(pn_field.get_num_pn(),6);
 
-    //EXPECT_EQ(pn_field.get_field()[0].get_vertices(),{9,56,75259,295,25,27});
+    std::vector<Point> pn_point{{9,56},{75259, 259},{25, 27}};
+    EXPECT_EQ(pn_field.get_field()[0].get_num_vertices(),3);
+    EXPECT_EQ(pn_field.get_field()[0].get_vertices(),pn_point);
+
+    pn_point={{10, 567}, {4, 23}, {75259, 259}};
+    EXPECT_EQ(pn_field.get_field()[1].get_num_vertices(),3);
+    EXPECT_EQ(pn_field.get_field()[1].get_vertices(),pn_point);
+
+    pn_point={{11, 567}, {4, 23}, {75259, 259}};
+    EXPECT_EQ(pn_field.get_field()[2].get_num_vertices(),3);
+    EXPECT_EQ(pn_field.get_field()[2].get_vertices(),pn_point);
+
+    pn_point={{12, 567}, {4, 23} ,{75259, 259}, {67, 68}, {39, 988}};
+    EXPECT_EQ(pn_field.get_field()[3].get_num_vertices(),5);
+    EXPECT_EQ(pn_field.get_field()[3].get_vertices(),pn_point);
+
+    pn_point={{13, 567}, {4, 23}};
+    EXPECT_EQ(pn_field.get_field()[4].get_num_vertices(),2);
+    EXPECT_EQ(pn_field.get_field()[4].get_vertices(),pn_point);
+
+    pn_point={{34, 34}};
+    EXPECT_EQ(pn_field.get_field()[5].get_num_vertices(),1);
+    EXPECT_EQ(pn_field.get_field()[5].get_vertices(),pn_point);
 }
+
 
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
+    //::testing::InitGoogleMock(&argc, argv);
 
     return RUN_ALL_TESTS();
 }
--- tests/test_readfile.cpp
+++ tests/test_readfile.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "../readfile.h"
-
 
 TEST(edit_file_test,start_end_space) {
     std::string input ="475485";
@@ -56,9 +54,6 @@ TEST(edit_file_test,total_results) {
     std::string input="48 \n            fdi    5\n343       f 34 5885\n 31 \n3983 \n     494\n";
     ASSERT_TRUE(edit_file(input)==" 48 fdi 5 343 f 34 5885 31 3983 494 ");
 }
-// test: find_key,get_coords, input_polygons,intersect_polygon_field_final
-// test: mult_vector,intersection, is_intersected,all class polygon functions
-// test: operator overloading, length, sign, abs
 
 TEST(find_key_test,different_keys){
     std::string input= " 03 9405 03 94";
@@ -184,7 +179,6 @@ TEST(get_coords_test,some_extreme_cases){
 
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
 
     return RUN_ALL_TESTS();
 }
