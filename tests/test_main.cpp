3c3aab1939fd33dc0cb1207d164abc60f50054cc
Андрей <elkabor@mail.ru>
2024-03-26 20:32:59 +0300

    mnogo testov but 12 not working and issue about 16th

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -90,8 +90,108 @@ TEST(intersect_polygon_field_final_test,triangle_inside_test3){
     EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
 
 }
+TEST(intersect_polygon_field_final_test,one_point_test4){
+    std::string input = "03 1 1 5 1 5 6 03 5 3 8 4 7 6";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
 
+    std::vector<Point> pn_points{{5,3}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,two_points_test5){
+    std::string input = "03 1 1 5 1 5 6 03 3 2 7 2 7 6";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
 
+    std::vector<Point> pn_points{{3,2},{5,2},{5,4}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,one_line_test6){
+    std::string input = "03 1 4 5 4 3 6 03 2 4 4 4 3 2";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{2,4},{4,4}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,four_points_test7){
+    std::string input = "03 2 1 5 1 5 5 03 2 2 6 2 2 5";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{2.75,2},{5,2},{5,2.75},{3.92,3.56}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,three_points_test8){
+    std::string input = "03 1 4 6 4 4 8 03 3 2 6 2 6 8";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{4,4},{6,4},{5,6}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,no_intersection_test9){
+    std::string input = "03 2 2 6 2 4 4 03 6 4 10 4 8 6";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{2, 2}, {6, 2}, {4, 4}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,five_points_test10){
+    std::string input = "03 6 3 6 6 9 3 03 5 5 9 1 9 5";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{6, 4},{7, 3}, {9, 3},{7, 5}, {6, 5}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,six_points_test11){
+    std::string input = "03 6 3 6 6 9 3 03 8 2 5 5 8 5";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{6, 4},{7, 3}, {8, 3}, {8, 4},{7, 5}, {6, 5}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+
+TEST(intersect_polygon_field_final_test,many_triangles_test12){
+    std::string input = "03 4 8 20 8 10 2 03 6 8 18 12 19 5 03 6 6 16 6 12 12 03 6 4 14 2 14 12 03 8 4 16 4 8 12";//неверно,должно быть{ 9.25 7.25, 13.8 6.2, 12 8, 10 8 }
+    //input = "03 4 8 20 8 10 2 03 4 8 18 12 19 5 03 6 6 16 6 12 12 03 6 4 14 2 14 12 03 8 4 16 4 8 12";//вообще не считает,должно быть {{9, 7},{14, 6}, {12, 8},{10,8}};
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{9, 7},{14, 6}, {12, 8},{10,8}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+
+TEST(intersect_polygon_field_final_test,triangle_and_intersection_test15){
+    std::string input = "03 2 2 6 2 4 4 03 6 4 10 4 8 6 03 7 5 9 5 8 4";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{7, 5}, {8, 4}, {9, 5}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
+TEST(intersect_polygon_field_final_test,many_same_triangles_and_intersection_test16){
+    std::string input = "03 1 1 3 1 2 3 03 1 1 3 1 2 3 03 1 1 3 1 2 3 03 5 2 8 2 6 4 03 6 1 8 1 6 3";
+    input= edit_file(input);
+    PolygonField pn_field;
+    pn_field.input_polygons(input);
+
+    std::vector<Point> pn_points{{6, 2},{7, 2}, {6, 3}};
+    EXPECT_EQ(pn_field.intersect_polygon_field_final().get_vertices(),pn_points);
+}
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
     //::testing::InitGoogleMock(&argc, argv);
