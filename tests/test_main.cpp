b21146965d1b7cbf175715ec929098aff9f48e39
VVroli <gaisinagaika@gmail.com>
2024-03-27 16:40:31 +0300

    line_tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,8 +3,8 @@
 //#include <gmock/gmock.h>
 #include "../main.h"
 
-// test: mult_vector,intersection, is_intersected,all class polygon functions
-// test: operator overloading, length, sign, abs
+// test: mult_vector,all class polygon functions
+// test: operator overloading, abs
 //main test #14 intersect_polygon_field_final
 TEST(input_polygons,test1){
     std::string input=" 03 9 56 75259 259 25 27 03 10 567 4 23 75259 259 25 27 03 11 567 4 23 75259 259 67 47 47 57 05 12 567 4 23 75259 259 67 68 39 988 90 67 03 03 13 567 4 23 5 03 14 03 34 34 ";
@@ -38,7 +38,7 @@ TEST(input_polygons,test1){
 }
 
 TEST(intersect_polygon_field_final_test,void_space_test1){
-    std::string input = "";
+    std::string input;
     input= edit_file(input);
     PolygonField pn_field;
     pn_field.input_polygons(input);
@@ -92,68 +92,75 @@ TEST(intersect_polygon_field_final_test,triangle_inside_test3){
 
 TEST(is_intersected_test, intersection_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 5};
-    Point c{2, 1};
-    Point d{4, 5};
+    Point a{1, 4}, b{6, 5}, c{2, 1}, d{4, 5};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d),true);
 }
 TEST(is_intersected_test, common_endpoint_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 4};
-    Point c{6, 2};
-    Point d{6, 6};
+    Point a{1, 4}, b{6, 4}, c{6, 2}, d{6, 6};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d),true);
 }
 TEST(is_intersected_test, non_intersection_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 5};
-    Point c{2, 1};
-    Point d{4, 3};
+    Point a{1, 4}, b{6, 5}, c{2, 1}, d{4, 3};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d), false);
 }
 TEST(is_intersected_test, parallel_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 4};
-    Point c{2, 3};
-    Point d{5, 3};
+    Point a{1, 4}, b{6, 4}, c{2, 3}, d{5, 3};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d),false);
 }
 TEST(is_intersected_test, horizontal_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 4};
-    Point c{3, 4};
-    Point d{3, 10};
+    Point a{1, 4}, b{6, 4}, c{3, 4}, d{3, 10};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d),false);
 }
 TEST(is_intersected_test, one_straight_line_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 4};
-    Point c{7, 4};
-    Point d{8, 4};
+    Point a{1, 4}, b{6, 4}, c{7, 4}, d{8, 4};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d),false);
 }
 TEST(is_intersected_test, lines_match_test) {
     PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 4};
-    Point c{1, 4};
-    Point d{6, 4};
-    ASSERT_EQ(pn_field.is_intersected(a,b,c,d),false);
-} //выводит не пересечение
-TEST(is_intersected_test, one_point_test) {
-    PolygonField pn_field;
-    Point a{1, 4};
-    Point b{6, 4};
-    Point c{2, 3};
-    Point d{2, 3};
+    Point a{1, 4}, b{6, 4}, c{1, 4}, d{6, 4};
     ASSERT_EQ(pn_field.is_intersected(a,b,c,d),false);
 }
+TEST(is_intersected_test, one_point_test) {
+    PolygonField pn_field;
+    Point a{1, 4}, b{6, 4}, c{2, 3}, d{2, 3};
+    ASSERT_EQ(pn_field.is_intersected(a,b,c,d),false);
+}
+
+TEST(intersection_test, segments_intersect) {
+    PolygonField pn_field;
+    Point a{1, 4}, b{6, 4}, c{3, 2}, d{3, 7};
+    std::vector<Point> result = pn_field.intersection(a, b, c, d);
+    ASSERT_EQ(result.size(), 1);
+    ASSERT_EQ(result[0].x, 3.0);
+    ASSERT_EQ(result[0].y, 4.0);
+}
+TEST(intersection_test, non_segments_intersect) {
+    PolygonField pn_field;
+    Point a{3, 2}, b{4, 5}, c{6, 4}, d{4, 6};
+    std::vector<Point> result = pn_field.intersection(a, b, c, d);
+    ASSERT_EQ(result.size(), 0);
+}
+
+TEST(length_test, test1) {
+    Point p{3.0, 4.0};
+    EXPECT_EQ(length(p), 5.0);
+}
+
+TEST(sign_test, positive_number) {
+    EXPECT_EQ(sign(10), 1);
+}
+TEST(SignTest, negative_number) {
+    EXPECT_EQ(sign(-7), -1);
+}
+TEST(SignTest, zero) {
+    EXPECT_EQ(sign(0.0), 1);
+}
+
 
 
 int main(int argc, char **argv){
